37547a8b3bbac38944efe46775064ef2f8f803a0
Lorna <lorna@dxw.com>
2022-09-12 17:42:15 +0100

    Import referrals from Google Sheets
    We mock Referral.worksheet to return example data, covering potential
    duplication.
    
    We filter out referrals which haven't opted in.

--- random-referral/app/models/referral.rb
+++ random-referral/app/models/referral.rb
@@ -1,4 +1,31 @@
+require "google_drive"
+
 class Referral
+  def self.worksheet
+    service_account_io = OpenStruct.new(read: "{
+      \"type\": \"service_account\",
+      \"project_id\": \"#{ENV["GOOGLE_DRIVE_PROJECT_ID"]}\",
+      \"private_key\": \"#{ENV["GOOGLE_DRIVE_PRIVATE_KEY"]}\",
+      \"client_email\": \"#{ENV["GOOGLE_DRIVE_CLIENT_EMAIL"]}\"
+    }")
+
+    session = GoogleDrive::Session.from_service_account_key(service_account_io)
+    session.spreadsheet_by_key(ENV.fetch("GOOGLE_DRIVE_SPREADSHEET_ID")).worksheets[0]
+  end
+
+  def self.all
+    worksheet.rows
+      .select { |row|
+        # only use links from folks who've opted in
+        row[3] == "Y"
+      }.map { |row|
+        # columns are zero-indexed
+        Referral.new(
+          code: row[2]
+        )
+      }
+  end
+
   attr_reader :code
 
   def initialize(code:)
--- random-referral/spec/models/referral_spec.rb
+++ random-referral/spec/models/referral_spec.rb
@@ -12,4 +12,41 @@ RSpec.describe Referral, type: :model do
       expect(subject.code).to eql("12356789")
     end
   end
+
+  describe ".all" do
+    before do
+      allow(Referral).to receive(:worksheet) {
+        OpenStruct.new(rows: [
+          ["Who (optional)", "Service", "Link / code", "Include in public site?"],
+          ["Sam", "Service 1", "Code for Sam", "Y"],
+          ["Mas", "Service 1", "Code for Mas", "Y"],
+          ["", "Service 1", "Code for unknown", "Y"],
+          ["Sam", "Service 2", "Code for Sam", "Y"],
+          ["Bob", "", "Code for Bob", "Y"],
+          ["", "", "Code for double unknown", "Y"],
+          ["Alice", "Service 3", "Code for Alice", ""],
+          ["Charlie", "Service 4", "Code for Charlie", "N"]
+        ])
+      }
+    end
+
+    it "returns an array of Referrals" do
+      Referral.all.each do |referral|
+        expect(referral).to be_instance_of(Referral)
+      end
+    end
+
+    it "creates a Referral for a row of the worksheet" do
+      allow(Referral).to receive(:new)
+      Referral.all
+      expect(Referral).to have_received(:new).with(code: "Code for Bob")
+    end
+
+    it "only returns referrals which have opted in to be public" do
+      referrals = Referral.all
+      expect(referrals.count).to eq(6)
+      expect(referrals).not_to include(an_object_having_attributes(code: "Code for Alice"))
+      expect(referrals).not_to include(an_object_having_attributes(code: "Code for Charlie"))
+    end
+  end
 end
